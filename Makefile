d63d6927d41393788009463e60f324b02f390724
Thomas Manu <tomkm22@gmail.com>
2018-01-25 20:17:17 +0000

    ezsh listens for input repeatedly

--- Makefile
+++ Makefile
@@ -174,6 +174,7 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
+	_ezsh\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
--- ezsh.c
+++ ezsh.c
@@ -1,8 +1,24 @@
 // ezsh.c
+#include "types.h"
+#include "user.h"
 
 int main(int argc, char* argv[]) {
-    while (true) {
-        printf(2, "EZ$ ");
+    static char buff[100];
+    int buffSize = sizeof(buff);
+    
+    while (1) {
+        printf(1, "EZ$ ");
+        memset(buff, 0, buffSize);
+        gets(buff, buffSize);
+        if ((buff[0] == 'e' && buff[1] == 'x' && buff[2] == 'i' && buff[3] == 't') || buff[0] == 'q') {
+            printf(1, "Exiting ezsh...\n");
+            exit();
+        }
+        else {
+            printf(1, "%s", buff);
+        }
+            
     }
     exit();
+    return 0;
 }
